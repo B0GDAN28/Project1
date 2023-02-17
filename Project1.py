1642f4d0971cbc99c666f0265c2adaa975d0759f
bogda <bogdan_opris2000@yahoo.com>
2023-02-17 11:28:51 +0200

    Projcet1 Version 2

--- Project1.py
+++ Project1.py
@@ -1,1 +1,6 @@
-print("Hello,World!")
+# print("Hello,World!")
+Name = input("Introduce-ti numele dumneavoastra:")
+height = input('introduce-ti inaltimea dumneavoastra')
+weight= input('introduce=ti greutatea dumneavoastra')
+print("Buna ziua " + Name)
+print("Aveti inaltimea de:"+ height +" si greutatea de:" +weight)
