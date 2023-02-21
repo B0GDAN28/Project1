d3144cb9d832f22f4cc75633a7f2816a99deacc6
bogda <bogdan_opris2000@yahoo.com>
2023-02-21 15:10:10 +0200

    Projcet1 Version 2.3

--- Project1.py
+++ Project1.py
@@ -2,6 +2,7 @@
 Name = input("Introduce-ti numele dumneavoastra:")
 Height = input('introduce-ti inaltimea dumneavoastra')
 Weight = input('introduce=ti greutatea dumneavoastra')
-message="Buna ziua {name}. Aveti inaltimea de {height} si greutatea de {weight}".format(name=Name,height=Height, weight=Weight)
+#message="Buna ziua {name}. Aveti inaltimea de {height} si greutatea de {weight}".format(name=Name,height=Height, weight=Weight)
+message=f"Buna ziua {Name}.Aveti inaltimea de {Height} M si greutatea de {Weight} KG"
 print(message)
 
